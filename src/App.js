3066455b1e091cb145d23f1730a6ca81364ab277
Callum McNeilage <31269757+Sunthas515@users.noreply.github.com>
2021-10-07 20:42:23 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { React, useState } from 'react';
+import { Input } from 'reactstrap'
 
 function App() {
   const [input, setInput] = useState('');
   return (
     <div className="App">
       <h1>CYA Cancer Baton</h1>
-      <input type="text" name="runnerNames" value={input} onInput={e => setInput(e.target.value)}/>
+      <Input type="text" name="runnerNames" value={input} onInput={e => setInput(e.target.value)}/>
       <h2>{input}</h2>
     </div>
   );
