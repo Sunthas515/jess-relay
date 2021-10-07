95ec2424ef07141dfbeecfa737165e5dcec934be
Callum McNeilage <31269757+Sunthas515@users.noreply.github.com>
2021-10-07 20:37:32 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-class App extends Component {
-  render() {
-    rconst [input, setInput] = useState('');
-    return (
-      <div className="App">
-        <h1>CYA Cancer Baton</h1>
-        <Input type="text" name="runnerNames" value={input} onInput={e => setInput(e.target.value)}/>
-        <h2>{input}</h2>
-      </div>
-    );
-  }
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [input, setInput] = useState('');
+  return (
+    <div className="App">
+      <h1>CYA Cancer Baton</h1>
+      <Input type="text" name="runnerNames" value={input} onInput={e => setInput(e.target.value)}/>
+      <h2>{input}</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
