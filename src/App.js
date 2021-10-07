33e862afbb10712717fc4db7286bbffaef8b98e4
Callum McNeilage <31269757+Sunthas515@users.noreply.github.com>
2021-10-07 20:38:54 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>CYA Cancer Baton</h1>
-      <Input type="text" name="runnerNames" value={input} onInput={e => setInput(e.target.value)}/>
+      <input type="text" name="runnerNames" value={input} onInput={e => setInput(e.target.value)}/>
       <h2>{input}</h2>
     </div>
   );
