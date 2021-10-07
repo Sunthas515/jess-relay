2f24765e86db5a3d2bd4e3a68e3eda96796767a7
Callum McNeilage <31269757+Sunthas515@users.noreply.github.com>
2021-10-07 20:46:35 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import './App.css';
-import { React, useState } from 'react';
-import { Input } from 'reactstrap'
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
-function App() {
-  const [input, setInput] = useState('');
-  return (
-    <div className="App">
-      <h1>CYA Cancer Baton</h1>
-      <Input type="text" name="runnerNames" value={input} onInput={e => setInput(e.target.value)}/>
-      <h2>{input}</h2>
-    </div>
-  );
+class LambdaDemo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { loading: false, msg: null }
+  }
+
+  handleClick = api => e => {
+    e.preventDefault()
+
+    this.setState({ loading: true })
+    fetch("/.netlify/functions/" + api)
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, msg: json.msg }))
+  }
+
+  render() {
+    const { loading, msg } = this.state
+
+    return (
+      <div className="App">
+        <h1>CYA Cancer Baton</h1>
+        <Input type="text" name="runnerNames" value={input} onInput={e => setInput(e.target.value)} />
+        <h2>{input}</h2>
+      </div>
+    )
+  }
 }
 
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    )
+  }
+}
+
+export default App
